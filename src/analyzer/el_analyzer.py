3c5200df76c5a90f45c469669b8b8552dfabe5ae
Milanovic <aleksandar.milanovic@zuehlke.com>
2024-02-02 12:50:17 +0100

    initial setup

---
+++ src/analyzer/el_analyzer.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, round, col
+
+
+class ElAnalyzer:
+    def __init__(self, spark, df):
+        """
+        Initialize the ElAnalyzer class with a Spark session.
+
+        Parameters:
+        - spark: SparkSession
+        - df: intiial DataFrame
+        """
+        self.spark = spark
+        self.df = df
+
+    def calculate_averages(self, order_col="AVG_PTS_DIFF"):
+        """
+        Calculate the points scored and conceived per team
+        Parameters:
+        - order_col: Scolumns used for order by sorting
+
+        Returns:
+        - result_df: Spark DataFrame with columns 'team', 'AVG_PTS', 'AVG_OP_PTS'
+        """
+        avg_columns = [
+            round(avg(col), 1).alias(f"AVG_{col}") for col in ["PTS", "OP_PTS"]
+        ]
+        result_df = (
+            self.df.groupBy("team")
+            .agg(*avg_columns)
+            .withColumn("AVG_PTS_DIFF", col("AVG_PTS") - col("AVG_OP_PTS"))
+            .orderBy(order_col, ascending=False)
+        )
+        return result_df
